1c96813d2240852e6a85b300f9d7fe01364efd7c
David Wolfe <wolfe@sheepdoginc.ca>
2012-10-01 13:48:19 -0300

    Prepare sheepdog deploy for cheeseshop

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from distutils.core import setup
- 
+
 setup(
     name='django-database-files',
-    version='0.1',
-    description='A storage system for Django that stores uploaded files in the database.',
-    author='Ben Firshman',
-    author_email='ben@firshman.co.uk',
-    url='http://github.com/bfirsh/django-database-files/',
+    version='0.1.1-sd',
     packages=[
-        'database_files',
+        'database_files', 'database_files.migrations'
     ],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 ],
 )
